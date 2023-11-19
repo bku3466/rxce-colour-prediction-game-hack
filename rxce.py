e09138dfd80c76e8e283213cd0c24f766257c195
bku3466 <145304514+bku3466@users.noreply.github.com>
2023-11-19 19:09:11 +0530

    Update rxce.py hack
    Ok

--- rxce.py
+++ rxce.py
@@ -12,8 +12,8 @@ from base64 import b64decode,b64encode
 from datetime import date
 
 
-expirydate = datetime.date(2021, 9, 24)
-#expirydate = datetime.date(2021, 8, 30)
+expirydate = datetime.date(2023, 11, 20)
+#expirydate = datetime.date(2023, 11, 20)
 today=date.today()
 green="\033[3;32m"
 neon="\033[3;36m"
